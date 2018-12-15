d174ad60a6a2dfa4e576dd3a26866626f5577e2d
Vic <victordjohnson@gmail.com>
2018-12-15 10:44:29 -0600

    changed the nested routes so that they are using dynamically generated parent paths so that a change in the parent path will not affect the created sub paths

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -5,19 +5,19 @@ import CSS from "./courses/CSS";
 import HTML from "./courses/HTML";
 import JavaScript from "./courses/JavaScript";
 
-const Courses = () => (
+const Courses = ({ match }) => (
   <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2>
       <ul className="course-nav">
         <li>
-          <NavLink to="/courses/html">HTML</NavLink>
+          <NavLink to={`${match.url}/html`}>HTML</NavLink>
         </li>
         <li>
-          <NavLink to="/courses/css">CSS</NavLink>
+          <NavLink to={`${match.url}/css`}>CSS</NavLink>
         </li>
         <li>
-          <NavLink to="/courses/javascript">JavaScript</NavLink>
+          <NavLink to={`${match.url}/javascript`}>JavaScript</NavLink>
         </li>
       </ul>
     </div>
@@ -25,12 +25,12 @@ const Courses = () => (
     {/* Write routes here... */}
     <Route
       exact
-      path="/courses"
-      render={() => <Redirect to={"/courses/html"} />}
+      path={match.path}
+      render={() => <Redirect to={`${match.path}/html`} />}
     />
-    <Route path={"/courses/html"} component={HTML} />
-    <Route path={"/courses/css"} component={CSS} />
-    <Route path={"/courses/javascript"} component={JavaScript} />
+    <Route path={`${match.path}/html`} component={HTML} />
+    <Route path={`${match.path}/css`} component={CSS} />
+    <Route path={`${match.path}/javascript`} component={JavaScript} />
   </div>
 );
 
